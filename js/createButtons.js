76b6dbd243deff704076819dcaea30ee2346d915
Valdermartje <valdermartje@gmail.com>
2021-05-20 19:34:09 +0200

    added words in game

--- js/createButtons.js
+++ js/createButtons.js
@@ -1,12 +1,46 @@
-function createButton(buttonName, buttonClass, positionInBody) {
-    
+function createButton(buttonName, buttonClass, positionInBody, buttonFunction) {
+
     const button = document.createElement("BUTTON");
+
     button.innerHTML = buttonName;
     button.classList.add(buttonClass || "default");
+    button.addEventListener("click", buttonFunction);
 
     if (positionInBody) {
+
         positionInBody.appendChild(button);
     } else {
-        document.body.appendChild(button);
+        console.error("There is no position specified in the 'positionInBody' parameter of the createButton() function");
+        throw new Error("There is no position specified in the 'positionInBody' parameter of the createButton() function");
     }
 }
+
+/**
+ * function Button() {
+
+    const button = document.createElement("BUTTON");    
+}
+
+Button.prototype.setButtonName = function(buttonName) {
+    button.innerHTML = buttonName;
+}
+
+Button.prototype.setButtonClass = function(buttonClass) {
+    button.classList.add(buttonClass || "default");
+}
+
+Button.prototype.setPositionInBody = function(positionInBody) {
+
+    if (positionInBody) {
+
+        positionInBody.appendChild(button);
+    } else {
+        console.error("There is no position specified in the 'positionInBody' parameter of the createButton() function");
+        throw new Error("There is no position specified in the 'positionInBody' parameter of the createButton() function");
+    }
+}
+
+Button.prototype.setButtonFunction = function() {
+    button.addEventListener("click", buttonFunction);
+}
+ */
--- js/index.js
+++ js/index.js
@@ -1,9 +1,28 @@
+const WORDS_TO_CHOOSE = [    "abruptly",    "absurd",    "abyss",    "affix",    "askew",    "avenue",    "awkward",    "axiom",    "azure",    "bagpipes",    "bandwagon",    "banjo",    "bayou",    "beekeeper",
+    "bikini",    "blitz",    "blizzard",    "boggle",    "bookworm",    "boxcar",    "boxful",    "buckaroo",    "buffalo",    "buffoon",    "buxom",    "buzzard",    "buzzing",    "buzzwords",    "caliph",    "cobweb",
+    "cockiness",    "croquet",    "crypt",    "curacao",    "cycle",    "daiquiri",    "dirndl",    "disavow",    "dizzying",    "duplex",    "dwarves",    "embezzle",    "equip",    "espionage",    "euouae",    "exodus",
+    "faking",    "fishhook",    "fixable",    "fjord",    "flapjack",    "flopping",    "spritz",    "squawk",    "staff",    "strength",    "strengths",    "stretch",    "stronghold",    "stymied",    "subway",
+    "swivel",    "syndrome",    "thriftless",    "thumbscrew",    "topaz",    "transcript",     "transgress",     "transplant",     "triphthong",     "twelfth",     "twelfths",     "unknown",     "unworthy",     "unzip", 
+    "uptown",     "vaporize",     "vixen",     "vodka",     "voodoo",    "vortex",   "voyeurism",    "walkway",    "waltz",    "wave",    "wavy",    "waxy",    "wellspring",    "wheezy",    "whiskey",    "whizzing",    "whomever",    "wimpy"
+];
+
 const LETTERS_IN_ALFABET = ["A","B", "C,", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+
+const container = document.querySelector(".container");
 
 
 window.addEventListener("load", function() {
 
-    createButton("Start game")
+    createButton(
+        buttonName = "Start game", 
+        buttonClass =  null,
+        positionInBody = container,
+        buttonFunction = function(){ console.log("clicked") }
+    )
+
+    // const startGameButton = new Button();
+    // startGameButton.setButtonName("Start game"); 
 
     console.log("loaded");
 })
